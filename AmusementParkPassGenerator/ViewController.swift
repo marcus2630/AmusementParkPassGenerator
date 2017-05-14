2d74a7c60a8d077487eb93dbfd702255465dd1d1
Marcus Klausen <marcus@Marcuss-MacBook-Pro.local>
2017-05-14 18:27:06 +0200

    removed instances from VC

--- AmusementParkPassGenerator/ViewController.swift
+++ AmusementParkPassGenerator/ViewController.swift
@@ -12,18 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        // Creating employee (manager) instance with profile
-        do {
-            let manager = try Employee(as: .manager, withInformation: .init(employeeWithFirstName: "Martin", lastName: "Enghave", street: "Wonderstreet", city: "Taastrup", state: "Copenhagen", zip: 1620))
-        } catch ProfileError.InvalidData(let description) {
-            print("\(description)")
-        } catch let error {
-            print("Something else went wrong")
-        }
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
